1a71b14d8212fcb5719641d49a4ea4081c8cb2f4
root <root@coldstorage.ihep.ac.cn>
2017-09-18 10:18:20 +0800

    change test

--- change.sh
+++ change.sh
@@ -2,9 +2,9 @@
 
 for file in `ls|grep eos`;
 do
-oldname=$file
-#newname=$(echo $file|sed 's#.cern##')
-newname=$(echo $file|sed 's#eos#jes#')
-echo $newname
-mv $oldname $newname
+	oldname=$file
+	#newname=$(echo $file|sed 's#.cern##')
+	newname=$(echo $file|sed 's#eos#jes#')
+	echo $newname
+	mv $oldname $newname
 done
